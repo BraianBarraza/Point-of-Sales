1a47b4f65e4b900e7e4b201112b2127914f15b7a
Braian <braian_019@hotmail.com>
2025-07-15 16:29:26 +0200

    Form styles added

--- formkit.config.js
+++ formkit.config.js
@@ -1,8 +1,25 @@
-import { fr } from '@formkit/i18n'
+import { generateClasses } from '@formkit/themes'
 
 const config = {
-  locales: { fr },
-  locale: 'fr',
+  config: {
+    classes: generateClasses({
+      global: {
+        label: 'block mb-1 font-bold text-lg',
+        message: 'text-red-500 mb-5',
+        wrapper: 'space-y-2 mb-3',
+        input: 'w-full p-3 border border-gray-300 rounded text-gray-700 placeholder-gray-400',
+      },
+      file: {
+        noFile: 'block my-2',
+        fileItem: 'hidden',
+      },
+      submit:{
+        input: '$reset bg-green-400 hover:bg-green-500 w-full p-2 font-bold uppercase ' +
+          'disable:opacity-50',
+      },
+
+    }),
+  },
 }
 
 export default config
--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -1,22 +1,28 @@
 <script setup>
 import Link from '@/components/Link.vue'
+import { FormKitIcon } from '@formkit/vue'
 </script>
 
 <template>
   <div>
-    <Link to="products"> Back</Link>
+    <Link to="products">Back</Link>
 
     <h1 class="text-4xl font-black my-10">Add A New Product</h1>
 
     <div class="flex justify-center bg-white shadow rounded-lg">
       <div class="mt-10 p-10 w-full 2xl:w-2/4">
-        <FormKit type="form" submit-label="Add Product">
+
+        <FormKit type="form"
+                 submit-label="Add Product"
+                 :actions="false"
+        >
+
           <FormKit
             type="text"
             label="Name"
             name="name"
             placeholder="Product Name"
-            validate="required"
+            validation="required"
             :validation-messages="{required: 'Field name is required'}"
           />
           <FormKit
@@ -24,9 +30,9 @@ import Link from '@/components/Link.vue'
             label="image"
             name="image"
             placeholder="Product image"
-            validate="required"
+            validation="required"
             :validation-messages="{required: 'Field image is required'}"
-            accept="jpg,jpeg,png"
+            accept=".jpg"
             multiple="true"
           />
           <FormKit
@@ -34,7 +40,7 @@ import Link from '@/components/Link.vue'
             label="Category"
             name="category"
             placeholder="Product Category"
-            validate="required"
+            validation="required"
             :validation-messages="{required: 'Field category is required'}"
             :options="[1,2,3]"
           />
@@ -43,7 +49,7 @@ import Link from '@/components/Link.vue'
             label="Price"
             name="price"
             placeholder="Product price"
-            validate="required"
+            validation="required"
             :validation-messages="{required: 'Field price is required'}"
             min="1"
           />
@@ -52,9 +58,12 @@ import Link from '@/components/Link.vue'
             label="Availability"
             name="availability"
             placeholder="Product Availability"
-            validate="required"
+            validation="required"
             :validation-messages="{required: 'Field Availability is required'}"
             min="0"
+          />
+          <FormKit
+          type="submit"
           />
 
         </FormKit>
